9ca5f7e484d2570a616237fa180652de7c35057d
Menna33 <mennalotfy33@gmail.com>
2023-10-06 15:40:04 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * then in uppercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+        char ch;
+        for (ch='a';ch<='z';ch++)
+        {
+                if (ch!='e'&&ch!='q')
+                putchar(ch);
+        }
+        putchar('\n');
+        return 0;
+}
