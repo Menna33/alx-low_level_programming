cc63b7686427c48e6c5df4861df319c236c5c502
Menna33 <mennalotfy33@gmail.com>
2023-10-10 23:00:19 +0300

    adding abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Write a function that prints the last digit of a number
+ * print_last_digit - prints the last digit of integer
+ * @n: integer to get the last digit of it
+ * Return: the last integer of it
+ */
+int print_last_digit(int n) 
+{
+int last_digit= n%10;
+putchar(last_digit);
+return (last_digit);
+}
