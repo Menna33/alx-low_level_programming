58335eabac2ffdac79260bc00a0507500f621a80
Menna33 <mennalotfy33@gmail.com>
2023-10-10 22:23:05 +0300

    adding sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_sign - print sign of the integer
+ * @n: integer to be verified
+ * Return: 0 or 1 or -1
+ * .
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');    
+return (1);
+}
+else if (n==0)
+{
+putchar('0');    
+return (0);
+}
+else
+{
+putchar('-');    
+return (-1);   
+}
+
+}
