c832dbc415fa636ab4c7a59850dd521f2f38274a
Menna33 <mennalotfy33@gmail.com>
2023-10-10 22:05:52 +0300

    edit for betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * islower - 
- *
+ * _islower - checks if the input is lowercase.
+ * @c: character to be verified
  * Return: 1 if lowercase , 0 if uppercase.
  */
 int _islower(int c)
 {
-if (c >=97 && c<=122)
- return 1;
-return 0;
+if (c >= 97 && c <= 122)
+return (1);
+return (0);
 }
