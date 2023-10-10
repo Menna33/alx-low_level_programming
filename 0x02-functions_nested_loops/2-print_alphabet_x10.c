8d606048aa58ea11ae9721d4f6579402a2d1a085
Menna33 <mennalotfy33@gmail.com>
2023-10-10 15:32:46 +0300

    mm

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c < 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
