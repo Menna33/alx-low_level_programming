80d1a03c6bb6a7ea9d5888cd887f6c9545558eee
Menna33 <mennalotfy33@gmail.com>
2023-10-10 22:11:33 +0300

    edit for betty style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * isalpha - checks for alphabetic character
- *
- * Return: 1 if lowercase , 0 if uppercase.
+ * @c: character to be verified
+ * Return: 0 or 1
+ * .
  */
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
- return (1);
+return (1);
 return (0);
 }
