af467d2129256541eac13e8156e500a43bad4e44
Menna33 <mennalotfy33@gmail.com>
2023-10-11 06:59:30 +0300

    adding 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,30 @@
 int add (int a, int b)
 {
 int i;
+int j;
+int mul;
 for (i = 0; i < 10; i++)
 {
-int j;
-
 for (j = 0; j < 10; j++)
+mul = j * i;
+if (j == 0)
 {
-_putchar(i * j + '0');
+_putchar(mul + '0');
+}
+if (mul < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(mul + '0');
+}
+else if (mul >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((mul / 10) + '0');
+_putchar((mul % 10) + '0');
+}
 }
 _putchar('\n');
-}
 }
