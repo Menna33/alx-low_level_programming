2af6be8bbd3f31e0b1599b8dcf90a486d1195830
Menna33 <mennalotfy33@gmail.com>
2023-10-11 09:54:40 +0300

    printing the minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ int i;
 int j;
 for (i = 0; i < 24; i++)
 {
+for (j = 0; j < 60; j++)
+{
 _putchar((i / 10)+'0');
 _putchar((i % 10)+'0');
 _putchar(':');
-for (j = 0; j < 60; j++)
-{
 _putchar((j / 10)+'0');
 _putchar((j % 10)+'0');
 _putchar('\n');
