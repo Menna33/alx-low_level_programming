21b7be27fb138814bda5ada6de7157ad285823f0
Menna33 <mennalotfy33@gmail.com>
2023-10-11 09:38:06 +0300

    printing the minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the minutes of the day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+int i;
+int j;
+for (i = 0; i < 24; i++)
+{
+if (i % 10 == i)
+{
+_putchar('0');
+}
+_putchar('i');
+_putchar(':');
+for (j = 0; j < 60; j++)
+{
+if (j % 10 == j)
+{
+_putchar('0');
+}
+_putchar('j');
+}
+_putchar('/n');
+}
+}
