e40401e68c8164158191467f206d36b173317780
Menna33 <mennalotfy33@gmail.com>
2023-10-11 06:48:04 +0300

    adding 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * times_table - prints the times table 
- * 
+ * times_table - prints the times table
+ *
  * Return: void
  */
 void times_table(void)
 {
 int i;
-for (i=0; i<10; i++)
+for (i = 0; i < 10; i++)
 {
-    int j;
-    for(j=0; j<10; j++)
-    {
-        
-        _putchar(i*j);
-    }
-    _putchar('\n');
+int j;
+
+for (j = 0; j < 10; j++)
+{
+_putchar(i * j + '0');
+}
+_putchar('\n');
 }
 }
