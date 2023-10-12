143ba9b3c82493b9c6829a168c2b496ed5296788
Menna33 <mennalotfy33@gmail.com>
2023-10-12 11:02:06 +0300

    adding is upper

---
+++ 0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the letter is uppercase or lowercase
+ *
+ * Return: 1 or 0.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+return (0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # alx-low_level_programming
 This is alx-low-level-programming
+This is more functions and nested loops
