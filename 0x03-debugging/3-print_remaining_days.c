5ecfb01d6e3475d6d23e5287a76cb2c7a0824de4
Menna33 <mennalotfy33@gmail.com>
2023-10-12 23:57:36 +0300

    mm

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,8 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-int leap_year = 0;
 if ((year % 4 == 0 || year % 400 == 0) && (year % 100 != 0))
 {
-leap_year = 1;
 if (month >= 2 && day >= 60)
 {
 day++;
@@ -26,7 +24,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day == 60 && !leap_year)
+if (month == 2 && day == 60 )
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
