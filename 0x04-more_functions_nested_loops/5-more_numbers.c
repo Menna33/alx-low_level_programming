73319f73fa6f506ba18b006658959518766366c1
Menna33 <mennalotfy33@gmail.com>
2023-10-12 23:21:23 +0300

    editing the code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (i >= 10)
+if (j >= 10)
 {
-_putchar(i / 10 + '0');
+_putchar(j / 10 + '0');
 }
-_putchar(i % 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
