9efa42593d9dc605133a281584ceefa6e346a0b7
Menna33 <mennalotfy33@gmail.com>
2023-10-12 13:50:30 +0300

    adding fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int i;
 
-for (i = 0; i < 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 printf("%s", "FizzBuzz");
@@ -18,6 +18,8 @@ else if (i % 5 == 0)
 printf("%s", "Buzz");
 else
 printf("%d", i);
+if (i != 100)
+printf(" ");
 }
 _putchar('/n');
 return (0);
