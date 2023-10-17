9e46ec02f0731705da889ecd65115dae80618bcb
Menna33 <mennalotfy33@gmail.com>
2023-10-17 08:01:13 +0300

    calculate the value of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of string
+ * @s: argument string
+ * Return: length of given string
+ */
+
+int _strlen(char *s)
+{
+int count = 0;
+while (*(s-count) != '\0')
+{
+count++;
+}
+return count;
+}
