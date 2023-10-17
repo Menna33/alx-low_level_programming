8ae91bb5af3583566923fb1d7c9fb08d0634f687
Menna33 <mennalotfy33@gmail.com>
2023-10-17 08:22:16 +0300

    calculate the value of string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+int length = 0;
+while (s[i] != '\0')
+{
+length++;
+i++;
+}
+while (s[length] != s[0])
+{
+_putchar(s[length]);
+length--;
+}
+_putchar('\n');
+}
