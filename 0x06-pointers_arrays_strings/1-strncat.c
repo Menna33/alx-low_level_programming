7b9aa339edc460500aee79a0ad1835a2a2d93706
Menna33 <mennalotfy33@gmail.com>
2023-10-19 10:18:51 +0300

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,15 +12,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int dest_length = 0;
-while (dest[i] != '\0')
+while (dest[dest_length] != '\0')
 {
 dest_length++;
 }
 while (i < n)
 {
-dest[dest_length - 1 + i] = src[i];
+dest[dest_length + i] = src[i];
 i++;
 }
-dest[dest_length - 1 + i] = '\0';
+dest[dest_length + i] = '\0';
 return (dest);
 }
