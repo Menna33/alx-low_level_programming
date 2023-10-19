cd32bcfdb22656197d9c70154121bfa4c0ea0da8
Menna33 <mennalotfy33@gmail.com>
2023-10-19 14:19:00 +0300

    modyifing the code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ int i, j;
 char small_letters[] = {'a', 'e', 'o', 't', 'l'};
 char capital_letters[] = {'A', 'E', 'O', 'T', 'L'};
 char numbers[] = {'4', '3', '0', '7', '1'};
-for (j = 0; j != '\0'; j++)
+for (j = 0; s[j] != '\0'; j++)
 {
 for (i = 0; i < 5; i++)
 {
