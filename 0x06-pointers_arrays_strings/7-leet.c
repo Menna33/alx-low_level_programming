f44b8ee5f8e8fafec01a1f04c659d5e4760294f6
Menna33 <mennalotfy33@gmail.com>
2023-10-19 10:12:59 +0300

    editing the code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,12 +9,12 @@
 char *leet(char *s)
 {
 int i, j;
-char small_letters[] = {'a', 'e', 'o', 't', 'l', '\0'};
-char capital_letters[] = {'A', 'E', 'O', 'T', 'L', '\0'};
-char numbers[] = {'4', '3', '0', '7', '1', '\0'};
-for (j = 0; j == '\0'; j++)
+char small_letters[] = {'a', 'e', 'o', 't', 'l'};
+char capital_letters[] = {'A', 'E', 'O', 'T', 'L'};
+char numbers[] = {'4', '3', '0', '7', '1'};
+for (j = 0; j != '\0'; j++)
 {
-for (i = 0; i <= 5; i++)
+for (i = 0; i < 5; i++)
 {
 if (s[j] == small_letters[i] || s[j] == capital_letters[i])
 s[j] = numbers[i];
