e68aee670dc9a70107247579f50cf1eda48484b2
Menna33 <mennalotfy33@gmail.com>
2023-10-19 09:31:07 +0300

    encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13 method
+ * @s: argument string
+ * Return: string encoded
+ */
+
+char *rot13(char *s)
+{
+int i, j;
+char input_letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output_letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (j = 0; s[j] != '\0'; j++)
+{
+for (i = 0; i < 26; i++)
+{
+if (s[j] == input_letters[i])
+s[j] = output_letters[i];
+}
+}
+return (s);
+}
