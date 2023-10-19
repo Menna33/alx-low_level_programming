e9ac3943db8edcfa54c075c7b61f04e3e42eeac4
Menna33 <mennalotfy33@gmail.com>
2023-10-19 13:53:01 +0300

    modyfing the code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,15 @@ while (dest[dest_length] != '\0')
 {
 dest_length++;
 }
-while (i < n)
+while (i < n && src[i] != '\0')
 {
 dest[dest_length + i] = src[i];
 i++;
 }
+if (i < n)
+{
 dest[dest_length + i] = '\0';
+}
+
 return (dest);
 }
