1cc8d8d975fb868760473b8fd48d55c2641e1f71
Menna33 <mennalotfy33@gmail.com>
2023-10-25 06:05:46 +0300

    nn

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,9 +15,9 @@ void print_chessboard(char (*a)[8])
 int i, j;
 for (j = 0; j < 8; j++)
 {
-for (i = 0; a[i][j] != '\0'; i++)
+for (i = 0; i < 8; i++)
 {
-_putchar(a[i][j]);
+_putchar(a[j][i]);
 }
 _putchar('\n');
 }
