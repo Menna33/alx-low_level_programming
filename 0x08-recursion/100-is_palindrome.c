cc70033925c405d1cb50bb82f1f03f521b9c433b
Menna33 <mennalotfy33@gmail.com>
2023-10-25 15:41:52 +0300

    is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,38 @@
+
+#include "main.h"
+
+/**
+ * length - calculate the length of string
+ *
+ * @s: string to calculate the length of it
+ *
+ * Return: the length of string
+ */
+
+int length(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+/**
+ * is_palindrome - calculate the  value of x raised to the power of y
+ *
+ * @s: string to check if it is palindrome or not
+ *
+ * Return:  1 or 0
+ */
+
+int is_palindrome(char *s)
+
+{
+if (*s == '\0')
+return (1);
+if (*s != *(s + length(s) - 1))
+return (0);
+return (is_palindrome(s + 1));
+}
