c6a39aa25f9f6c56dff1a87694482e1d80d8e542
Menna33 <mennalotfy33@gmail.com>
2023-10-27 22:45:28 +0200

    nn

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the program's name
+ *
+ * @argc: number of elements of array argv
+ * @argv: array
+ * 
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
