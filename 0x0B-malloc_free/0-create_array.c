b559f6a27f03bb35f4285a3eb700bb5b3b51a0fd
Menna33 <mennalotfy33@gmail.com>
2023-10-31 11:10:20 +0200

    editing the code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 {
 char *arr;
 unsigned int i;
+if (size == 0)
+return (NULL);
 arr = (char *) malloc(size * sizeof(char));
 if (arr == NULL)
 return (NULL);
