e62d230ccbe368c4f196a1f825efafd8e0673e17
Menna33 <mennalotfy33@gmail.com>
2023-10-31 12:49:31 +0200

    adding the code

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid -concatenates two strings
+ *
+ * @width: grid width
+ * @height: grid height
+ *
+ * Return: the grid of zeros
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **arr;
+if (width <= 0 || height <= 0)
+return (NULL);
+arr = malloc(sizeof(int *) * height);
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+return (NULL);
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+}
