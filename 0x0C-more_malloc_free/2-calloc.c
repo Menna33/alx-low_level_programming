750988e54901b71075de9f6a94a596fcc500c78a
Menna33 <mennalotfy33@gmail.com>
2023-11-03 21:31:58 +0200

    adding the code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 int *arr, i;
 if (nmemb == 0 || size == 0)
 return (NULL);
-arr = malloc(sizeof(unsigned int) * size);
+arr = malloc(sizeof(int) * size);
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
