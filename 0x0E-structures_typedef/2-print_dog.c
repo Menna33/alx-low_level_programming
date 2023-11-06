3b1e5a440fbcfc5114abc9128b6b5635913c9673
Menna33 <mennalotfy33@gmail.com>
2023-11-06 12:52:12 +0200

    printing the dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,11 +17,11 @@ return;
 if (d->name == NULL)
 printf("Name:%s\n", " (nil)");
 else
-printf("Name:%s\n", d->name);
-printf("Age:%f\n", d->age);
+printf("Name = %s\n", d->name);
+printf("Age = %f\n", d->age);
 if (d->owner == NULL)
-printf("Owner:%s\n", " (nil)");
+printf("Owner = %s\n", " (nil)");
 else
-printf("Owner:%s\n", d->owner);
+printf("Owner = %s\n", d->owner);
 
 }
