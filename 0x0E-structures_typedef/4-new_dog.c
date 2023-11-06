05b771e19cea3b2394904ed06be0809a144ade6c
Menna33 <mennalotfy33@gmail.com>
2023-11-07 01:12:34 +0200

    edit the code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,32 @@
 #include <string.h>
 
 /**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: the string to be copied
+ *
+ * Return: pointer to the array
+ */
+
+char *_strdup(char *str)
+{
+char *new_str;
+unsigned int i;
+if (str == NULL)
+return (NULL);
+new_str = malloc(sizeof(char) * (strlen(str) + 1));
+if (new_str == NULL)
+return (NULL);
+
+for (i = 0; i < strlen(str); i++)
+{
+*(new_str + i) = *(str + i);
+}
+return (new_str);
+}
+
+/**
  * new_dog - create new dog
  * @name: dog's name
  * @age: dog's age
@@ -16,21 +42,19 @@ dog_t *new_one;
 new_one = malloc(sizeof(dog_t));
 if (new_one == NULL)
 return (NULL);
-new_one->name = malloc(sizeof(char) * strlen(name) + 1);
+new_one->name = _strdup(name);
 if (new_one->name == NULL)
 {
 free(new_one);
 return (NULL);
 }
-new_one->owner = malloc(sizeof(char) * strlen(owner) + 1);
+new_one->owner = _strdup(owner);
 if (new_one->owner == NULL)
 {
 free(new_one->name);
 free(new_one);
 return (NULL);
 }
-new_one->owner = owner;
-new_one->name = name;
 new_one->age = age;
 return (new_one);
 }
