5804fbf6585da514f84a7b1b39190dcfc0314488
Menna33 <mennalotfy33@gmail.com>
2023-11-09 00:13:30 +0200

    editing the code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,20 +6,19 @@
  * int_index - searches for an integer
  * @array: the array to executes the function on its elements
  * @size: the size of the array
- * @cmp: function to compare 2 strings
+ * @cmp: function to compare 2 integers
  * Return : index of the element or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
-
 {
 int i;
 if (size <= 0)
 return (-1);
 if (array == NULL || cmp == NULL)
-return;
+return (-1);
 for (i = 0; i < size; i++)
 {
-if (cmp(*(array + i)) != 0)
+if (cmp(array[i]) != 0)
 {
 return (i);
 }
