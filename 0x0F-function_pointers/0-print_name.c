c8db731731b5dfa5692a9a3f5dc045c40997f900
Menna33 <mennalotfy33@gmail.com>
2023-11-08 23:52:49 +0200

    adding the code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
 /**
  * print_name - prints the name
  * @name: the name to be printed
- * @f : pointer to function to print the name
+ * @f : function to print the name
  * Return : void
  */
 void print_name(char *name, void (*f)(char *))
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function on every array element
+ * @array: the array to executes the function on its elements
+ * @size: the size of the array
+ * @action: the function
+ * Return : void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}}
