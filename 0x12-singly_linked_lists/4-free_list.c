f37c896fc11a88e293fca8a539b47113097e5847
Menna33 <mennalotfy33@gmail.com>
2023-11-16 15:49:34 +0200

    adding code of 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,30 +1,13 @@
 #include "lists.h"
 /**
- * add_node_end - adds a new node at the end of a list_t list.
+ * free_list -frees a list_t list.
  * @head: the first node in the list
- * @str: the data of the node
- * return: the address of the new element, or NULL if it failed
+ * return: void
  */
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-list_t *new, *last;
-new = malloc(sizeof(list_t));
-if (new == NULL)
-{
-return (NULL);
-}
-new->str = strdup(str);
-new->len = sizeof(str);
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-}
-last = *head;
-while(last)
-{
-last = last->next;
-}
-last->next = new;
-return (*head);
+if (head == NULL)
+return;
+free_list(head->next);
+free(head);
 }
