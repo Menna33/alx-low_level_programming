37c5589a6529cc237573255b6bd60ecf35406556
Menna33 <mennalotfy33@gmail.com>
2023-11-16 15:01:47 +0200

    adding the code

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This signly linked lists directory
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * struct list_t - singly linked list
+ * @str: string that holds data
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_t{
+char *str;
+int len;
+struct list_t *next;
+}list_t;
+typedef long unsigned size_t;
+size_t print_list(const list_t *h);
+#endif
