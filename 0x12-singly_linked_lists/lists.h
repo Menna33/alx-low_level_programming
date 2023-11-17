a951293b7b44e07b0b6d401f5c6868fd43f43dde
Menna33 <mennalotfy33@gmail.com>
2023-11-17 22:45:37 +0200

    editing the code

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,12 +13,13 @@
  * Description: singly linked list node structure
  */
 
-typedef struct list_t{
+typedef struct list_t
+{
 char *str;
 int len;
 struct list_t *next;
-}list_t;
-typedef long unsigned size_t;
+} list_t;
+typedef unsigned long size_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
