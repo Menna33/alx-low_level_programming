984855c5cf76e10d6e47f051580f4665fb9aa353
Menna33 <mennalotfy33@gmail.com>
2023-11-22 15:44:10 +0200

    adding the7 file

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - pops the list's head
+ * @head: the list
+ * @index: the desired index
+ * Return: the node at the given index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+int count = 0;
+listint_t *tmp = head;
+while (count != index && tmp)
+{
+tmp = tmp->next;
+count++;
+}
+return (tmp);
+return (NULL);
+}
