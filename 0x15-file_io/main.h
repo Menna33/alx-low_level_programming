c44e8c3008ed28fa2111b179e340a5bc3381bb3d
Menna33 <mennalotfy33@gmail.com>
2023-11-28 17:26:00 +0200

    fix errors

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include <stdlib.h>
 #include<sys/types.h>
 ssize_t read_textfile(const char *filename, size_t letters);
-#endif MAIN_H
+#endif
